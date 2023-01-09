91c41751e9dda80f2e19b9182da98311930e8c11
KiZZuLA <kirillprogames@mail.ru>
2023-01-09 12:14:15 +0300

    Insted

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         StringBuilder sb1 = new StringBuilder("hello");
-        sb1.append(" I am").append(" Kirill.");
+        sb1.append(" I' m").append(" Kirill.");
         System.out.println(sb1);
     }
 }
