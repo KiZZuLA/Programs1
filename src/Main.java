6ae4037bfa6459a52e9e54bda0faa1332050a794
KiZZuLA <kirillprogames@mail.ru>
2023-01-09 11:59:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        StringBuilder sb1 = new StringBuilder("hello");
+        sb1.append(" I am").append(" Kirill.");
+        System.out.println(sb1);
+    }
+}
